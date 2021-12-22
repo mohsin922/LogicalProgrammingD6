97d8cb9a6746033ac7ae0592dc9671c18853fefe
unknown <mohsinpeer22@gmail.com>
2021-12-22 21:21:25 +0530

    Monthly Payment

--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -1,37 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
 
-
-namespace LogicalProgramming
+namespace Payment
 {
-    class TemperatureConvertor
+    class Util
     {
-        public void Fah(double C)
+        public void MonthlyPayemt(int P, int y, int R)
         {
-            double F;
-            F = (C * 1.8) + 32;
-            Console.WriteLine("temperature in Fahrenheit:{0}", F);
-        }
-        public void Cel(double FH)
-        {
-            double Ce;
-            Ce = ((FH - 32)*5/9 );
-            Console.WriteLine("Temperature in Celsius : {0}", Ce);
+            double n, r;
+            
+            
+            n = 12 * y;
+            r = R / (12 * 100);
+
+            double monthpay = P * r / ( 1 - (Math.Pow ((1 + r), -n )));
+            Console.WriteLine("the payment is {0}", monthpay);
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter temperature in deg C to get in Fahrenheit ");
-            double C = Convert.ToDouble(Console.ReadLine());
-            TemperatureConvertor u = new TemperatureConvertor();
-            u.Fah(C);
+            Console.WriteLine("Enter the principle amount");
+            int P = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter temperature in deg F to get in Celsius");
-            double FH = Convert.ToDouble(Console.ReadLine());
-            TemperatureConvertor s = new TemperatureConvertor();
-            s.Cel(FH);
+            Console.WriteLine("Enter no. of  years ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter rate of interest");
+            int R = Convert.ToInt32(Console.ReadLine());
+
+            Util u = new Util();
+            u.MonthlyPayemt(P, y, R);
+
+            
         }
     }
 }
